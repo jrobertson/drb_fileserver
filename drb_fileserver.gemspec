5a37c6f56e3dd8b2e05700bb37be159dda913110
Your Name <you@example.com>
2018-01-25 10:04:00 +0000

    feature: A base path can now be set.

--- drb_fileserver.gemspec
+++ drb_fileserver.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'drb_fileserver'
-  s.version = '0.1.0'
+  s.version = '0.1.1'
   s.summary = 'A DRb server to reads or write local files.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/drb_fileserver.rb']
--- lib/drb_fileserver.rb
+++ lib/drb_fileserver.rb
@@ -8,20 +8,30 @@ class DRbFileServer
 
   class FileX
 
+    def initialize(path='.')
+      
+      @path = path
+      
+    end
+    
     def read(filename)
-      File.read filename
+      
+      File.read File.join(@path, filename)
+      
     end
 
     def write(filename, s)
-      File.write filename, s
+      
+      File.write File.join(@path, filename), s
+      
     end
 
   end
 
-  def initialize(host: 'localhost', port: '61010')
+  def initialize(host: 'localhost', port: '61010', path: '.')
 
     @host, @port = host, port
-    @file = FileX.new
+    @file = FileX.new path
 
   end
 

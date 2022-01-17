1546bedd00ebaa132e17843e202cabceec6cf860
James Robertson <james@jamesrobertson.me.uk>
2022-01-17 21:09:40 +0000

    feature: Added file operation chmod.

--- drb_fileserver.gemspec
+++ drb_fileserver.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'drb_fileserver'
-  s.version = '0.2.3'
+  s.version = '0.3.0'
   s.summary = 'A DRb server to read or write local files.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/drb_fileserver.rb']
   s.signing_key = '../privatekeys/drb_fileserver.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/drb_fileserver'
 end
--- lib/drb_fileserver.rb
+++ lib/drb_fileserver.rb
@@ -10,23 +10,27 @@ require 'fileutils'
 class DRbFileServer
 
   class FileX
-    
+
 
     def initialize(path='.')
-      
+
       @path = path
-      
+
     end
-    
+
+    def chmod(permissions, filename)
+      FileUtils.chmod permissions, File.join(@path, filename)
+    end
+
     def cp(path, path2)
       #puts 'cp: ' + [File.join(@path, path), File.join(@path, path2)].inspect
       FileUtils.cp File.join(@path, path), File.join(@path, path2)
-    end        
-    
+    end
+
     def directory?(filename)
       File.directory? File.join(@path, filename)
-    end    
-    
+    end
+
     def exists?(filename)
       File.exists? File.join(@path, filename)
     end
@@ -34,44 +38,44 @@ class DRbFileServer
     # path can include a wildcard and the switch -ltr
     #
     def ls(rawpath)
-      
+
       path, switch = rawpath.split(/\s+/,2)
       wildcard = path.include?('*') ? '' : '*'
       path = File.join(path, wildcard) if File.exists? File.join(@path, path)
-      
-      a = Dir[File.join(@path, path)].map do |x|               
-        
+
+      a = Dir[File.join(@path, path)].map do |x|
+
         File.basename(x)
       end
-      
+
       if switch == '-ltr' then
-        a.sort_by {|x| File.mtime(File.join(@path, File.dirname(path), x)) } 
+        a.sort_by {|x| File.mtime(File.join(@path, File.dirname(path), x)) }
       else
         a
       end
-      
-    end    
-    
+
+    end
+
     def mkdir(name)
       FileUtils.mkdir File.join(@path, name)
     end
-    
+
     def mkdir_p(path)
       FileUtils.mkdir_p File.join(@path, path)
     end
-    
+
     def mv(path, path2)
       FileUtils.mv File.join(@path, path), File.join(@path, path2)
-    end    
-    
+    end
+
     def read(filename)
       File.read File.join(@path, filename)
     end
-    
+
     def rm(filename)
       FileUtils.rm File.join(@path, filename)
-    end     
-    
+    end
+
     def stop()
       puts 'stopping DFS service ...'
       DRb.stop_service
@@ -81,19 +85,19 @@ class DRbFileServer
     def write(filename, s)
       File.write File.join(@path, filename), s
     end
-    
-    # zips a file. Each array items contains a filename, and content to 
+
+    # zips a file. Each array items contains a filename, and content to
     # be written to the file.
     #
     def zip(filename_zip, a)
-      
+
       Zip::File.open(filename_zip, Zip::File::CREATE) do |x|
 
-        a.each do |filename, buffer| 
+        a.each do |filename, buffer|
           x.get_output_stream(filename) {|os| os.write buffer }
         end
 
-      end         
+      end
     end
 
   end
